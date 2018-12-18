1a221bc5556aaddd018edeb10f38dfb69bf355a1
shuye520com <shuye520com@163.com>
2018-12-18 11:51:22 +0800

    添加日志和shell运行

--- src/main/java/com/sec/ilearn/api/GeneratePyApi.java
+++ src/main/java/com/sec/ilearn/api/GeneratePyApi.java
@@ -4,6 +4,8 @@ import com.sec.ilearn.util.JsonResult;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Date;
 
 
 @RequestMapping("api")
 @Controller
 public class GeneratePyApi {
+
+    private static final Logger logger = LogManager.getLogger(GeneratePyApi.class);
 
     @ApiOperation(value = "生成py文件", notes = "生成py文件")
     @ApiImplicitParams({
@@ -26,19 +32,21 @@ public class GeneratePyApi {
     })
     @RequestMapping(value = "generatepy",method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<JsonResult> searchSysUserAddr(@RequestParam(value = "jsonparam")String jsonparam){
+    public ResponseEntity<JsonResult> generatePy(@RequestParam(value = "jsonparam")String jsonparam){
         JsonResult r = new JsonResult();
         try {
             r.setCode("200");
             r.setMsg("生成成功！");
             r.setData("....生成xxx文件内容： "+jsonparam);
             r.setSuccess(true);
+            logger.info(new Date().toLocaleString()+"调用生成py文件成功");
         } catch (Exception e) {
             r.setCode("500");
             r.setMsg("生成失败！");
             r.setData(null);
-            r.setSuccess(true);
+            r.setSuccess(false);
             e.printStackTrace();
+            logger.error(new Date().toLocaleString()+"调用生成py文件失败");
         }
         return ResponseEntity.ok(r);
     }
@@ -50,14 +58,27 @@ public class GeneratePyApi {
     })
     @RequestMapping(value = "runpy",method = RequestMethod.GET)
     @ResponseBody
-    public String testRunPy(@RequestParam(value = "path")String path){
+    public ResponseEntity<JsonResult> testRunPy(@RequestParam(value = "path")String path){
+        JsonResult r = new JsonResult();
         String res = "default";
         try {
             res = runPy(path);
+            r.setCode("200");
+            r.setMsg("生成成功！");
+            r.setData(res);
+            r.setSuccess(true);
+            logger.info(new Date().toLocaleString()+"运行py文件成功");
+
         } catch (IOException e) {
             e.printStackTrace();
+            r.setCode("500");
+            r.setMsg("生成失败！");
+            r.setData(res);
+            r.setSuccess(false);
+            e.printStackTrace();
+            logger.error(new Date().toLocaleString()+"运行py文件失败");
         }
-        return res;
+        return ResponseEntity.ok(r);
     }
 
     //运行py文件
@@ -87,11 +108,49 @@ public class GeneratePyApi {
         if("".equals(error)){
 
         }else{
-//            logger.error("名称："+name+"\n运行："+pathName+":"+error,"system","系统异常");
-            System.out.println("名称："+name+"\n运行："+pathName+":"+error+" system:系统异常");
+            logger.error("名称："+name+"\n运行："+pathName+":"+error,"system","系统异常");
+//            System.out.println("名称："+name+"\n运行："+pathName+":"+error+" system:系统异常");
             return "名称："+name+"\n运行："+pathName+":"+error+" system:系统异常";
         }
-        System.out.println(pathName + " " +result);
+//        System.out.println(pathName + " " +result);
+        logger.info(new Date().toLocaleString()+"运行py文件成功  "+pathName + " " +result);
         return pathName + " 运行结果： " +result;
+    }
+
+    @ApiOperation(value = "执行shell命令", notes = "执行shell命令")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "param", value = "相关参数", required = true, dataType = "String",paramType = "query")
+    })
+    @RequestMapping(value = "runshell",method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<JsonResult> testShell(@RequestParam(value = "param")String param){
+        JsonResult r = new JsonResult();
+        String[] cmds = {"/bin/sh","-c","ps -ef|grep java"};
+        String result = "";
+        Process pro = null;
+        try {
+            pro = Runtime.getRuntime().exec(cmds);
+            pro.waitFor();
+            InputStream in = pro.getInputStream();
+            BufferedReader read = new BufferedReader(new InputStreamReader(in));
+            String line = null;
+            while((line = read.readLine())!=null){
+                result += line;
+            }
+            r.setCode("200");
+            r.setMsg("执行shell命令成功！");
+            r.setData(result);
+            r.setSuccess(true);
+            logger.info(new Date().toLocaleString()+"执行shell命令成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            r.setCode("500");
+            r.setMsg("执行shell命令失败！");
+            r.setData(null);
+            r.setSuccess(false);
+            e.printStackTrace();
+            logger.error(new Date().toLocaleString()+"执行shell命令失败");
+        }
+        return ResponseEntity.ok(r);
     }
 }
